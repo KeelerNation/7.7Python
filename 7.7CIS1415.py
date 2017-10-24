73765676f7eb13de0e90a43d59333ba00266ba03
KeelerNation <CubedMC.eu@gmail.com>
2017-10-24 09:14:29 -0500

    more

--- 7.7CIS1415.py
+++ 7.7CIS1415.py
@@ -1,2 +1,8 @@
 title = input('Enter a title for the date:\n')
-print('You entered: %s' % title)
+print('You entered: %s\n' % title)
+
+header1 = input('Enter the column 1 Header:\n')
+print('You entered %s' % header1)
+
+header2 = input('Enter the column 2 Header:\n')
+print('You entered %s' % header2)
