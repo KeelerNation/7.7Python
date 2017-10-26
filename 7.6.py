8064b396577df6c5bd388205c48d3d8a54882443
jl7164ut <33062505+jl7164ut@users.noreply.github.com>
2017-10-26 10:49:34 -0500

    ol

--- 7.6.py
+++ 7.6.py
@@ -1,12 +1,16 @@
 def stringOperations(string):
-    if re.search(',',string):
+    if ',' in string:
         splitWords = string.split(',')
         print('First word:',splitWords[0].strip())
         print('Second word:',splitWords[1].strip())
         print('')
     else:
+<<<<<<< HEAD
     	print('Error: No comma in string.\n')
     return
+=======
+        print('Error: No comma in string.\n')
+>>>>>>> 0991df25c8ace51da4289cf8372e63f30aaec362
 
 while True:
     string = input('Enter input string:\n')
