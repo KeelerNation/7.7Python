7008a2e05679b8d7304191e40b1d8323179d1798
KeelerNation <CubedMC.eu@gmail.com>
2017-10-26 08:00:58 -0500

    ex

---
+++ ch7ex.py
@@ -0,0 +1,9 @@
+string = input('Enter your full name:\n')
+
+new = string.split(' ')
+print(new)
+newstr1 = new[0]
+newstr2 = new[1]
+newstr3 = new[2]
+
+print('%s. %s. %s.' % (newstr1[0:1], newstr2[0:1], newstr3[0:1]))
