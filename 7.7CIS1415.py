19c23b9def6c08bfcf59d88e55f06718e5ef64bc
KeelerNation <CubedMC.eu@gmail.com>
2017-10-26 09:17:06 -0500

    working

--- 7.7CIS1415.py
+++ 7.7CIS1415.py
@@ -8,11 +8,50 @@ header2 = input('Enter the column 2 Header:\n')
 print('You entered %s\n' % header2)
 
 
-while True:
-    data = input('Enter a data point (-1 to stop input:\n')
-    if data == '-1':
-        break
-    data = data.split(',')
-    chart = {}
-    chart.update({data[0]: data[1]})
-    print(chart)
+
+
+
+
+
+
+data = ''
+
+chart = {}
+
+name_list = []
+
+while data!='-1':
+    good_input = False
+    while not good_input:
+        data = input('Enter a data point (-1 to stop input:\n')
+        if (data == '-1'):
+            break
+        elif ',' not in data:
+            print('Error: No comma in string.')
+
+        elif len(data.split(','))> 2:
+            print('Too many commas in input.')
+
+        else:
+            data2 = data.split(',')
+            dataa = data2[0]
+            datab = data2[1]
+            datab = datab.replace(' ', '')
+            chart.update({dataa: datab})
+            name_list.append(dataa)
+            print('Data string:', dataa)
+            print('Data integer:', datab)
+
+
+print(chart)
+print('Names', name_list)
+
+
+
+
+
+
+
+
+
+
