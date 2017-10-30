6629285f3d646bfcd7b04cf6096725f486ad0966
KeelerNation <CubedMC.eu@gmail.com>
2017-10-30 12:13:04 -0500

    working

--- 7.5AE.py
+++ 7.5AE.py
@@ -1,36 +1,45 @@
+
+
+def num(split1, split2):
+    old = split[1] + split[2]
+    nn = ''
+
+    for let in old:
+
+        if let == 'A' or let == 'B' or let == 'C':
+            new = '2'
+        elif let == 'D' or let == 'E' or let == 'F':
+            new = '3'
+        elif let == 'G' or let == 'H' or let == 'I':
+            new = '4'
+        elif let == 'J' or let == 'K' or let == 'L':
+            new = '5'
+        elif let == 'M' or let == 'N' or let == 'O':
+            new = '6'
+        elif let == 'P' or let == 'Q' or let == 'R' or let == 'S':
+            new = '7'
+        elif let == 'T' or let == 'U' or let == 'V':
+            new = '8'
+        elif let == 'W' or let == 'X' or let == 'Y' or let == 'Z':
+            new = '9'
+
+        nn = nn + new
+    return nn
+
 phonenumold = input("Enter a 10-character telephone number:\n")
 print(phonenumold)
 
 split = phonenumold.split('-')
-print(split)
+split1 = split[1]
+split2 = split[2]
 
 last4 = split[2]
 
 last_num = 0
 L = '-'
-nn = ''
-for let in last4:
+nn = num(split1, split2)
 
-    if let == 'A' or let == 'B' or let == 'C':
-        new = '2'
-    elif let == 'D' or let == 'E' or let == 'F':
-        new = '3'
-    elif let == 'G' or let == 'H' or let == 'I':
-        new = '4'
-    elif let == 'J' or let == 'K' or let == 'L':
-        new = '5'
-    elif let == 'M' or let == 'N' or let == 'O':
-        new = '6'
-    elif let == 'P' or let == 'Q' or let == 'R' or let == 'S':
-        new = '7'
-    elif let == 'T' or let == 'U' or let == 'V':
-        new = '8'
-    elif let == 'W' or let == 'X' or let == 'Y' or let == 'Z':
-        new = '9'
-
-    nn = nn + new
-
-print(split[0] + L + split[1] + L + nn)
+print(split[0] + L + nn[:3] + L + nn[3:])
 
 
 
