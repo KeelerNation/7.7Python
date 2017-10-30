2717337901ee76f6c13cf7e32b4f953861919381
KeelerNation <CubedMC.eu@gmail.com>
2017-10-30 11:29:57 -0500

    working

--- 7.9AE.py
+++ 7.9AE.py
@@ -5,8 +5,14 @@ def vol(string):
     return count
 
 
-#def con(string):
+def con(string):
+    c = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q',
+         'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'B', 'C', 'D', 'F', 'G', 'H',
+         'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z')
+    count = sum([1 for i in string if i in c])
 
+    return count
 
 string = input('Enter a string:\n')
-print(vol(string))
+print('Number of vowels:', vol(string))
+print('Number of consonants:', con(string))
